b466870140a1cd1958dff582194196771c446183
Plane-walker <a990990@163.com>
2021-09-23 14:06:34 +0800

    Peer MSP Generate. #3

--- backend/node_build.py
+++ backend/node_build.py
@@ -2,14 +2,35 @@ import subprocess
 import argparse
 
 
-def docker_compose(path: str, down: bool=False) -> subprocess.CompletedProcess:
-    '''
+def peer_msp_generate(crypto_base, peer_name, org_name, org_domain, ca_port):
+    org_home = f'{crypto_base}/organizations/peerOrganizations/{org_name}.{org_domain}'
+    peer_home = f'{org_home}/peers/{peer_name}.{org_name}.{org_domain}'
+    ca_tls_ca = f'{crypto_base}/organizations/fabric-ca/{org_name}/tls-cert.pem'
+    command = f'mkdir -p {peer_home};' \
+              f'fabric-ca-client register --caname ca-{org_name} --id.name {peer_name} --id.secret {peer_name}pw --id.type peer --tls.certfiles {ca_tls_ca};' \
+              f'fabric-ca-client enroll -u https://{peer_name}:{peer_name}pw@ca.{org_name}.{org_domain}:{ca_port} --caname ca-{org_name} -M {peer_home}/msp --csr.hosts {peer_name}.{org_name}.{org_domain} --tls.certfiles {ca_tls_ca};' \
+              f'cp {peer_home}/msp/cacerts/* {peer_home}/msp/cacerts/localhost-{ca_port}-ca-{org_name}.pem;' \
+              f'fabric-ca-client enroll -u https://{peer_name}:{peer_name}pw@ca.{org_name}.{org_domain}:{ca_port} --caname ca-{org_name} -M {peer_home}/tls --enrollment.profile tls --csr.hosts {peer_name}.{org_name}.{org_domain} --csr.hosts localhost --tls.certfiles {ca_tls_ca};' \
+              f'cp {org_home}/msp/config.yaml {peer_home}/msp/config.yaml;' \
+              f'cp {peer_home}/tls/tlscacerts/* {peer_home}/tls/ca.crt;' \
+              f'cp {peer_home}/tls/signcerts/* {peer_home}/tls/server.crt;' \
+              f'cp {peer_home}/tls/keystore/* {peer_home}/tls/server.key;' \
+              f'mkdir -p {org_home}/msp/tlscacerts;' \
+              f'cp {peer_home}/tls/tlscacerts/* {org_home}/msp/tlscacerts/ca.crt;' \
+              f'mkdir -p {org_home}/tlsca;' \
+              f'cp {peer_home}/tls/tlscacerts/* {org_home}/tlsca/tlsca.{org_name}.${org_domain}-cert.pem;' \
+              f'mkdir -p {org_home}/ca;' \
+              f'cp {peer_home}/msp/cacerts/* {org_domain}/ca'
+    subprocess.run(command, shell=True, stdout=subprocess.PIPE)
+
+
+def docker_compose(path: str, down: bool = False) -> subprocess.CompletedProcess:
+    """
     :param path: The relative path from main program or absolute path of docker-compose.yaml(including filename)
     :param down: True means `docker-compose down` and False means `docker-compose up`
-    '''
+    """
     command = f"docker-compose -f {path} up -d" if not down else f"docker-compose -f {path} down"
     return subprocess.run(command, shell=True, stdout=subprocess.PIPE)
-
 
 
 if __name__ == "__main__":
