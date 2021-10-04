827e8ede38721188125763d627893cada646e067
Plane-walker <a990990@163.com>
2021-10-04 11:39:55 +0800

    Fix create order msp bugs. #35

--- backend/main.py
+++ backend/main.py
@@ -44,35 +44,35 @@ def generate_ca(ca_id, ca_information, fabric_name, target_host, crypto_base):
     ssh.close()
 
 
-def generate_order_msp(order_id, order_information, crypto_base='/root/opt'):
+def generate_order_msp(order_id, order_information, ca_port, crypto_base):
     node_name, group_name, domain = order_id.split('.', 2)
     address = order_information['address']
-    node_port = address['port']
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     key_file = io.StringIO(address['sk'])
     private_key = paramiko.RSAKey.from_private_key(key_file)
     ssh.connect(hostname=address['host'], port=address['ssh_port'], username='root', pkey=private_key)
-    tls_cert_path = f'fabric-ca/{group_name}'
+    tls_cert_path = f'organizations/fabric-ca/{group_name}'
     stdin, stdout, stderr = ssh.exec_command(f'if [ ! -d {crypto_base}/{tls_cert_path} ]; then mkdir -p {crypto_base}/{tls_cert_path}; fi')
     stdout.channel.recv_exit_status()
     ftp_client = ssh.open_sftp()
-    ftp_client.put(f'{tls_cert_path}/tls-cert.pem', f'{crypto_base}/{tls_cert_path}')
+    ftp_client.put(f'{tls_cert_path}/tls-cert.pem', f'{crypto_base}/{tls_cert_path}/tls-cert.pem')
     file_name = 'node_build.py'
     ftp_client.put(file_name, f'{crypto_base}/{file_name}')
-    order_yaml_generator = OrderYamlGenerator()
-    file_name = order_yaml_generator.generate(order_id, group_name, node_name)
-    ftp_client.put(file_name, f'{crypto_base}/{file_name}')
-    ssh.exec_command(f'python node_build.py --func_name org_msp_generate "{crypto_base}" "{group_name}" "{domain}" "{node_port}"')
-    ssh.exec_command(f'python node_build.py --func_name peer_msp_generate "{crypto_base}" "{node_name}" "{group_name}" "{domain}" "{node_port}"')
+    stdin, stdout, stderr = ssh.exec_command(f'python {crypto_base}/node_build.py --func_name org_msp_generate {group_name} {domain} {ca_port} {crypto_base}')
+    stdout.channel.recv_exit_status()
+    # print(stdout.readlines(), stderr.readlines())
+    stdin, stdout, stderr = ssh.exec_command(f'python {crypto_base}/node_build.py --func_name peer_msp_generate {node_name} {group_name} {domain} {ca_port} {crypto_base}')
+    stdout.channel.recv_exit_status()
+    # print(stdout.readlines(), stderr.readlines())
     tls_ca_path = f'organizations/{group_name}.{domain}/tlsca'
     if not os.path.exists(tls_ca_path):
         os.makedirs(tls_ca_path)
-    ftp_client.get(f'{crypto_base}/{tls_ca_path}/tlsca.{group_name}.{domain}-cert.pem', tls_ca_path)
-    server_path = f'organizations/{group_name}.{domain}/orderers/{order_id}/tls'
+    ftp_client.get(f'{crypto_base}/{tls_ca_path}/tlsca.{group_name}.{domain}-cert.pem', f'{tls_ca_path}/tlsca.{group_name}.{domain}-cert.pem')
+    server_path = f'organizations/{group_name}.{domain}/peers/{order_id}/tls'
     if not os.path.exists(server_path):
         os.makedirs(server_path)
-    ftp_client.get(f'{crypto_base}/{server_path}/server.crt', server_path)
+    ftp_client.get(f'{crypto_base}/{server_path}/server.crt', f'{server_path}/server.crt')
     ftp_client.close()
 
 
@@ -98,9 +98,9 @@ def generate_peer(peer_id, peer_information, order_group_id, crypto_base='/root/
     peer_yaml_generator = PeerYamlGenerator()
     file_name = peer_yaml_generator.generate(peer_id, group_name, node_name)
     ftp_client.put(file_name, f'{crypto_base}/{file_name}')
-    ssh.exec_command(f'python node_build.py --func_name org_msp_generate "{crypto_base}" "{group_name}" "{domain}" "{node_port}"')
-    ssh.exec_command(f'python node_build.py --func_name peer_msp_generate "{crypto_base}" "{node_name}" "{group_name}" "{domain}" "{node_port}"')
-    ssh.exec_command(f'python node_build.py --func_name docker_compose "{file_name}"')
+    ssh.exec_command(f'python node_build.py --func_name org_msp_generate {group_name} {domain} {node_port} {crypto_base}')
+    ssh.exec_command(f'python node_build.py --func_name peer_msp_generate {node_name} {group_name} {domain} {node_port} {crypto_base}')
+    ssh.exec_command(f'python node_build.py --func_name docker_compose {file_name}')
     peer_path = f'organizations/{group_name}.{domain}'
     if not os.path.exists(peer_path):
         os.makedirs(peer_path)
@@ -122,17 +122,21 @@ def generate_order(order_id, order_information, fabric_name, channel_id, peer_gr
 
 def parse_json(network_topology_json):
     order_group_id = ''
+    order_ca_port = ''
     target_host = ''
     peer_group_ids = []
+    peer_ca_port = []
     for group_id, group_information in network_topology_json['groups'].items():
         if group_id.split('.', 1)[0] == 'orderer':
             order_group_id = group_id
+            order_ca_port = network_topology_json['nodes'][group_information['nodes']['ca']]['address']['fabric_port']
             target_host = network_topology_json['nodes'][network_topology_json['groups'][group_id]['nodes']['ca']]['address']['host']
         else:
             peer_group_ids.append(group_id)
+            peer_ca_port.append(network_topology_json['nodes'][group_information['nodes']['ca']]['address']['fabric_port'])
         generate_ca(group_information['nodes']['ca'], network_topology_json['nodes'][group_information['nodes']['ca']], network_topology_json['blockchains']['fabric-1']['name'], target_host, '/root/opt')
-    # for order_id in network_topology_json['groups'][order_group_id]['nodes']['orderer']:
-    #     generate_order_msp(order_id, network_topology_json['nodes'][order_id])
+    for order_id in network_topology_json['groups'][order_group_id]['nodes']['orderer']:
+        generate_order_msp(order_id, network_topology_json['nodes'][order_id], order_ca_port, '/root/opt')
     # for org_id in peer_group_ids:
     #     leader_peers_ids = network_topology_json['groups'][org_id]['nodes']['leader_peers']
     #     anchor_peers_ids = network_topology_json['groups'][org_id]['nodes']['anchor_peers']
